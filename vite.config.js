2c0e4be2d4b90464aeb925b8d1d003c242cf18e7
MacBook <macbook@MacBook-Air.local>
2024-03-11 11:17:37 -0500

    adding more pages

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+export default {
+    build: {
+      rollupOptions: {
+        input: {
+          index: 'index.html',
+          nosotros: 'nosotros.html',
+        proceso: 'proceso.html'
+        }
+      }
+    }
+  }
