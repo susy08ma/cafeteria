62adbcd147ad071c4fae3876d8a0888b58cdbe45
MacBook <macbook@MacBook-Air.local>
2024-03-18 10:09:55 -0500

    proyecto terminado

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,17 @@ export default {
         input: {
           index: 'index.html',
           nosotros: 'nosotros.html',
-        proceso: 'proceso.html'
+          proceso: 'proceso.html',
+          menu: 'menu.html',
+          contacto: 'contacto.html',
+          galeria: 'galeria.html'
+        }
+      }
+    },
+    css: {
+      preprocessorOptions: {
+        sass: {
+          sourceMap: true
         }
       }
     }
